0118b18746a44eaaa807fabf3dada03b4a0e5beb
spksoft <spkrsk.37@gmail.com>
2022-03-06 19:39:39 +0700

    make it get token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { publiclient } from 'chainauth-js-sdk';
 
+const setting = { apiKey: "aabbb" }
+publiclient.setSettings(setting);
 function App() {
+  const showNonce = async () => {
+    const nonce = await publiclient.getLoginNonce();
+    console.log(nonce);
+  }
+  const clickMe = async () => {
+    console.log("clicked");
+    const nonce = await publiclient.getLoginNonce();
+    console.log('nonce', nonce);
+    const v = await publiclient.getLoginSignature(nonce);
+    console.log('v', v);
+    const token = await publiclient.getTokenFromSigature(nonce, v);
+    console.log('token', token);
+  }
+  useEffect(() => {
+    showNonce();
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +38,7 @@ function App() {
         >
           Learn React
         </a>
+        <p onClick={clickMe}>Click me</p>
       </header>
     </div>
   );
